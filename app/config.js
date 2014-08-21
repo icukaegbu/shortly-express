d56d146835d44397211b5de721f4d6bf87cd8366
icukaegbu <icukaegbu@gmail.com>
2014-08-21 12:48:26 +0100

    Modified the Users table definition

--- app/config.js
+++ app/config.js
@@ -49,7 +49,9 @@ db.knex.schema.hasTable('users').then(function(exists) {
     db.knex.schema.createTable('users', function (user) {
       user.increments('id').primary();
       user.string('username');
-      user.string('hash');
+      // user.string('hash');
+      user.string('password');
+      user.string('salt');
       user.timestamps();
     }).then(function (table) {
       console.log('Created Table', table);
